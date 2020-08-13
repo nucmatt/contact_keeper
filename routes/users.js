6e7c7e66f8fa266fcf24d6ad440e678d48ac4f2e
Matthew Wessel <nucmatt@gmail.com>
2020-08-13 10:05:18 -0500

    Registered the route for a user to be created and added hashing for the user's created password.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 // Bring in express validator for error checking of the post requests received
 const { check, validationResult } = require('express-validator');
 
@@ -10,8 +11,8 @@ const User = require('../models/User');
 // @access      Public
 // Here the '/' refers to the api/users route, not the home page. This is because we created a route in server.js to automatically forward anything that includes api/users to this file.
 router.post(
-    '/',
-    // Everything in brackets here is for express validator. Don't forget to write the errors part of the validation below!
+	'/',
+	// Everything in brackets here is for express validator. Don't forget to write the errors part of the validation below!
 	[
 		check('name', 'Please add a name').not().isEmpty(),
 		check('email', 'Please include a valid email').isEmail(),
@@ -20,15 +21,48 @@ router.post(
 			'Please enter a password with 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		// res.send('Register a user...');
-        // res.send(req.body);
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
-        }
+		// res.send(req.body);
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		//  res.send('passed');
 
-        res.send('passed');
+		// Parse the body of the request
+		// Destructuring!
+		const { name, email, password } = req.body;
+
+		try {
+			// Here we check to see the entered email address already exists in the database. findOne is a Mongoose method. ES6 allows us to just enter email instead of email: email.
+			let user = await User.findOne({ email });
+
+			// If email already exists, return a bad request with a message
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists' });
+			}
+
+			// Create new User object (brought in from User model) if email is unique
+			user = new User({
+				name,
+				email,
+				password,
+			});
+
+			// This is for hashing the user's password. BCrypt requires a salt variable to tell it how many passes to hash the password. More hashes (the number in parentheses) means more security. Note that genSalt returns a promise so you must use await since we are using async/await instead of promises.
+			const salt = await bcrypt.genSalt(10);
+
+			// Here we hash the user object's password before adding it to the database.
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			res.send('User saved');
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
