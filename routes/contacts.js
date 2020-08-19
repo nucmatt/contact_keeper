e1e7409ca1f4a29001c7fc7e297b54bc6f1ea071
Matthew Wessel <nucmatt@gmail.com>
2020-08-19 11:12:02 -0500

    Updated the contacts.js route POST request to allow for adding a user to the database. Verified contact retrieval is user specific using Postman.

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,9 +28,39 @@ router.get('/', auth, async (req, res) => {
 // @route       POST api/contacts
 // @desc        Add new contacts
 // @access      Private
-router.post('/', (req, res) => {
-	res.send('Add contact');
-});
+router.post(
+	'/',
+	[auth, [check('name', 'Name is required').not().isEmpty()]],
+	async (req, res) => {
+		// res.send('Add contact');
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		// Destructuring!
+		const { name, email, phone, type } = req.body;
+
+		try {
+			// remember you have access to the user object via the auth middleware so you can assign the user's id to this contact to complete the Contact Schema.
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id,
+			});
+
+			// save method puts the newly created contact into the database.
+			const contact = await newContact.save();
+			// Then we return the contact back to the user so it is displayed in their contacts.
+			res.json(contact);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 // PUT updates an existing entry. To update the entry correctly you need to provide an identifier. That is what the /:id will be, the identifier for the contact to be updated.
 // @route       PUT api/contacts/:id
