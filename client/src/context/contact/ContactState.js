f3b1a0ce27bd327d76d2b23d1a4ebf426fc32f2d
Matthew Wessel <nucmatt@gmail.com>
2020-08-25 11:22:09 -0500

    Created context folder and initial scripting for React frontend UI.

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,67 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from ' ./contactReducer';
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_CONTACT,
+	FILTER_CONTACTS,
+	CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+	// initialState will eventually equal an empty array of contacts, to be filled from the user's database. For now we are using hardcoded contacts for initial buildout.
+	const initialState = {
+		contacts: [
+			{
+				id: 1,
+				name: 'Jill Johnson',
+				email: 'jill@gmail.com',
+				phone: '111-111-1111',
+				type: 'personal',
+			},
+			{
+				id: 2,
+				name: 'Sara Watson',
+				email: 'sara@gmail.com',
+				phone: '222-222-2222',
+				type: 'personal',
+			},
+			{
+				id: 3,
+				name: 'Harry White',
+				email: 'harry@gmail.com',
+				phone: '333-333-3333',
+				type: 'professional',
+			},
+		],
+	};
+
+	const [state, dispatch] = useReducer(contactReducer, initialState);
+
+	// Add Contact
+
+	// Delete Contact
+
+	// Set Current Contact
+
+	// Clear Current Contact
+
+	// Update Contact
+
+	// Filter Contacts
+
+	// Clear Filter
+
+    // In the return statement below, props.children will become any component that is rendered within the <ContactState> component. This makes it so that all those children will have access to the state of the ContactState component since it is a Context API Provider. In other words, the child components subscribe to the ContactState component's state and will update/rerender themselves whenever ContactState has it's state updated.
+	return (
+		<ContactContext.Provider value={{ contact: state.contacts }}>         
+			{props.children}
+		</ContactContext.Provider>
+	);
+};
+
+export default ContactState;
